6198837ade3c8683d526d086c86d8f3c5489da43
novy154 <slnowak154@gmail.com>
2015-01-04 11:22:22 +0100

    Added commands and events (without implementation yet)

---
+++ src/main/java/agh/bit/eventsbc/domain/eventproposal/commands/CompleteAndApproveAnalysingCommand.java
@@ -0,0 +1,13 @@
+package agh.bit.eventsbc.domain.eventproposal.commands;
+
+import lombok.Value;
+import lombok.experimental.Accessors;
+
+/**
+ * Created by novy on 04.01.15.
+ */
+
+@Value
+@Accessors(fluent = true)
+public class CompleteAndApproveAnalysingCommand {
+}
---
+++ src/main/java/agh/bit/eventsbc/domain/eventproposal/events/EventProposalApprovedEvent.java
@@ -0,0 +1,13 @@
+package agh.bit.eventsbc.domain.eventproposal.events;
+
+import lombok.Value;
+import lombok.experimental.Accessors;
+
+/**
+ * Created by novy on 04.01.15.
+ */
+
+@Value
+@Accessors(fluent = true)
+public class EventProposalApprovedEvent {
+}
