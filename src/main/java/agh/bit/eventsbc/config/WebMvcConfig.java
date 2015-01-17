5da5917fbe247e4e3c8edf2bd4deb00db932b786
Kamil Kulak <kulak.kam@gmail.com>
2015-01-17 01:08:20 +0100

    added and configured jackson, fulfilled dummy controller

--- src/main/java/agh/bit/eventsbc/config/WebMvcConfig.java
+++ src/main/java/agh/bit/eventsbc/config/WebMvcConfig.java
@@ -1,10 +1,30 @@
 package agh.bit.eventsbc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @EnableWebMvc
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jackson2HttpMessageConverter());
+    }
+
+    private HttpMessageConverter<?> jackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter();
+    }
 }
